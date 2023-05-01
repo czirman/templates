7835d00e9932933cded07c1d385c7148f7c20bf3
czirman <perseverance.void@poczta.pl>
2023-05-01 10:38:53 +0200

    Remove outdatted swagger and update necessary libs to fix errors

--- SwaggerSpringBoot/src/main/java/org/czirman/controllers/TestController.java
+++ SwaggerSpringBoot/src/main/java/org/czirman/controllers/TestController.java
@@ -1,7 +1,5 @@
 package org.czirman.controllers;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,10 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
-@Api(value = "Template controler")
 public class TestController {
 
-    @ApiOperation(value = "test only")
     @PostMapping(path ="/test")
     public ResponseEntity<String> test(/* final @RequestBody CreditAss cs */){
         return new ResponseEntity("ff", HttpStatus.OK);
