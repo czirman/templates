f26c1fd5aead2cdda7d6c296d314cefaac91bff0
czirman <mkucman@gmail.com>
2024-01-02 15:16:49 +0100

    Fix many bugs 2

--- SpringBoot/src/main/java/org/czirman/controllers/TestController.java
+++ SpringBoot/src/main/java/org/czirman/controllers/TestController.java
@@ -2,15 +2,16 @@ package org.czirman.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+// url http://localhost:8080/test
 public class TestController {
 
-    @PostMapping(path ="/test")
+    @GetMapping(path ="/test")
     public ResponseEntity<String> test(/* final @RequestBody CreditAss cs */){
         return new ResponseEntity("ff", HttpStatus.OK);
 
